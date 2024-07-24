f9cd8b4bd195dce1cc1bb4cf4168425eb648c408
Sami Sabir-Idrissi <sami_sabir-idrissi@epam.com>
2024-07-24 18:36:45 -0400

    gemini smoke test passed

---
+++ gemini_google_basics/gemini_flash.py
@@ -0,0 +1,26 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# Fetch the API key from environment variables
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+if GEMINI_API_KEY is None:
+    raise ValueError("GEMINI_API_KEY environment variable is not set")
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+response = model.generate_content("Give me python code to sort a list")
+
+# Check if the response contains a valid Part
+if response.parts:
+    print(response.text)
+else:
+    # Handle cases where no valid Part is returned
+    if response.candidate.safety_ratings:
+        print("Content generation was blocked due to safety filters.")
+        # You can print more detailed safety ratings here
+        print(response.candidate.safety_ratings)
+    else:
+        print("No valid content was generated.")
